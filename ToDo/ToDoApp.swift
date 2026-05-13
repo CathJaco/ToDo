cefcf8ab74ca40d5eca607714e1e16ee90b60166
Cathrine Jacobsen <cathrine.j@hotmail.com>
2026-05-13 20:25:44 +0200

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Cathrine Jacobsen on 13/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoTests/ToDoTests.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoTests.swift
+//  ToDoTests
+//
+//  Created by Cathrine Jacobsen on 13/05/2026.
+//
+
+import Testing
+
+struct ToDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
