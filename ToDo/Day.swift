c16107619de1f97832ca5018a9d69fb89c8abb88
Cathrine Jacobsen <cathrine.j@hotmail.com>
2026-05-18 21:55:23 +0200

    Controlled

--- ToDo/Day.swift
+++ ToDo/Day.swift
@@ -9,16 +9,26 @@ import SwiftUI
 
 struct Day: View {
     var dayName: String
+    @State var tasks: [String] = ["tekst", "noe nytt"]
+    
     var body: some View {
         GroupBox(label:
-                Label(dayName, systemImage: "building.columns")
-        ){
-            Text("test")
+                Label(dayName, systemImage: "heart")
+        ) {
+            List(tasks, id: \.self) { task in
+                Text(task)
+            }
+            Button("Add", systemImage: "plus", action: {
+                tasks.append("hæ")
+            })
+                .labelStyle(.iconOnly)
             
         }
     }
+
 }
 
 #Preview {
     Day(dayName: "MONDAY")
 }
+
