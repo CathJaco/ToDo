aef02874319538196f7c8e821556f3878be95641
Cathrine Jacobsen <cathrine.j@hotmail.com>
2026-08-04 21:43:19 +0200

    Brain toggle first try

---
+++ ToDo/OOM_Toggle.swift
@@ -0,0 +1,18 @@
+//
+//  OOM_Toggle.swift
+//  ToDo
+//
+//  Created by Cathrine Jacobsen on 04/08/2026.
+//
+
+import SwiftUI
+
+struct OOM_Toggle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OOM_Toggle()
+}
